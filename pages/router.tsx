35081df43c907fdf90b2515044ff96d34105501d
GeniferGonzalezSantos <genh_@hotmail.com>
2024-05-16 22:25:13 -0300

    add types

--- pages/router.tsx
+++ pages/router.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/navigation";
 
 export default function Router() {
   const router = useRouter();
-  router.push("/Profile_Id");
-  router.push("/Register");
+  if (typeof window !== "undefined") {
+    router.push("/Profile_Id");
+    router.push("/Register");
+  }
 }
